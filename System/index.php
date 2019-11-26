9ab264e2e9a000f17d3d10f7a302aa769d469415
mvnrsr <mivienrosario@gmail.com>
2019-11-27 01:15:54 +0800

    added admin2 theme for testing

--- System/index.php
+++ System/index.php
@@ -8,241 +8,212 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="src/css/main.css">
   </head>
   <body>
-    <nav class="navbar navbar-light bg-light justify-content-between">
-      <ul class="nav">
-        <li class="nav-item justify-content-end">
-          <a class="navbar-brand" href="#">SBO</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Hello, User</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="inc/logout.inc.php">Logout</a>
-        </li>
-      </ul>
-    </nav>
+    <div class="main-container">
+      <div class="col side-nav">
+        <ul>
+          <li>
+            <a href="#">Link 1</a>
+          </li>
+          <li>
+            <a href="#">Link 2</a>
+          </li>
+          <li>
+            <a href="#">Link 3</a>
+          </li>
+        </ul>
+      </div>
 
-    <h1 style="color: red">Note: No joined tables yet.</h1>
-    <h1>User Section</h1>
-    <form class="" action="inc/login.inc.php" method="post">
-      <ul>
-        <li>
-          <label for="">Username</label>
-          <input type="text" name="username">
-        </li>
-        <li>
-          <label for="">Password</label>
-          <input type="password" name="password">
-        </li>
-        <li>
-          <button type="submit" name="login">Log In</button>
-        </li>
-      </ul>
-    </form>
-<hr>
+      <div class="col ">
+        <h1>Student Section</h1>
+        <form class="" action="inc/insert.inc.php" method="post">
+          <ul>
+            <li>
+              <label for="studID">Student ID</label>
+              <input type="text" name="studID">
+            </li>
+            <li>
+              <label for="studID">Last Name</label>
+              <input type="text" name="lname">
+            </li>
+            <li>
+              <label for="studID">First Name</label>
+              <input type="text" name="fname">
+            </li>
+            <li>
+              <label for="studID">Middle Name</label>
+              <input type="text" name="mname">
+            </li>
+            <li>
+              <label for="studID">Address</label>
+              <input type="text" name="address">
+            </li>
+            <li>
+              <label for="studID">Contact Number</label>
+              <input type="text" name="num">
+            </li>
+            <li>
+              <button type="submit" name="saveStud">Add New Student</button>
+            </li>
+          </ul>
+        </form>
 
-  <h1>Student Registration</h1>
-  <form class="" action="" method="post">
-    <ul>
-      <li>
-        <label for=""></label>
-      </li>
-    </ul>
-  </form>
+        <table id="students" class="display">
+          <thead>
+            <tr>
+              <th>Student ID</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Contact Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              $sql = "SELECT * FROM sbo.student";
+              $result = mysqli_query($conn, $sql);
+              $resultCheck = mysqli_num_rows($result);
 
-<hr>
-<h1>Student Section</h1>
-    <form class="" action="inc/insert.inc.php" method="post">
-      <ul>
-        <li>
-          <label for="studID">Student ID</label>
-          <input type="text" name="studID">
-        </li>
-        <li>
-          <label for="studID">Last Name</label>
-          <input type="text" name="lname">
-        </li>
-        <li>
-          <label for="studID">First Name</label>
-          <input type="text" name="fname">
-        </li>
-        <li>
-          <label for="studID">Middle Name</label>
-          <input type="text" name="mname">
-        </li>
-        <li>
-          <label for="studID">Address</label>
-          <input type="text" name="address">
-        </li>
-        <li>
-          <label for="studID">Contact Number</label>
-          <input type="text" name="num">
-        </li>
-        <li>
-          <button type="submit" name="saveStud">Add New Student</button>
-        </li>
-      </ul>
-    </form>
+              if ($resultCheck > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                  echo '
+                    <tr>
+                      <td>'. $row['student_id'].'</td>
+                      <td>'. $row['last_name']. ', '. $row['first_name']. ' '. $row['middle_name'] . '</td>
+                      <td>'. $row['address'].'</td>
+                      <td>'. $row['contact_num'].'</td>
+                    </tr>';
+                }
+              }
+            ?>
+          </tbody>
 
-    <table id="students" class="display">
-      <thead>
-        <tr>
-          <th>Student ID</th>
-          <th>Name</th>
-          <th>Address</th>
-          <th>Contact Number</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-          $sql = "SELECT * FROM sbo.student";
-          $result = mysqli_query($conn, $sql);
-          $resultCheck = mysqli_num_rows($result);
+        </table>
 
-          if ($resultCheck > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-              echo '
-                <tr>
-                  <td>'. $row['student_id'].'</td>
-                  <td>'. $row['last_name']. ', '. $row['first_name']. ' '. $row['middle_name'] . '</td>
-                  <td>'. $row['address'].'</td>
-                  <td>'. $row['contact_num'].'</td>
-                </tr>';
-            }
-          }
-        ?>
-      </tbody>
+        <hr>
+        <h1>Emergency Contact Section</h1>
+        <form class="" action="inc/insert.inc.php" method="post">
+          <ul>
+            <li>
+              <label for="">Last Name</label>
+              <input type="text" name="em-lname">
+            </li>
+            <li>
+              <label for="">First Name</label>
+              <input type="text" name="em-fname">
+            </li>
+            <li>
+              <label for="">Middle Name</label>
+              <input type="text" name="em-mname">
+            </li>
+            <li>
+              <label for="">Contact Number</label>
+              <input type="text" name="em-num">
+            </li>
+            <li>
+              <label for="">Address</label>
+              <input type="text" name="em-address">
+            </li>
+            <li>
+              <button type="submit" name="em-save">Save Contact</button>
+            </li>
+          </ul>
+        </form>
 
-    </table>
+        <table id="emergency" class="display">
+          <thead>
+            <tr>
+              <th>Emergency Contact ID</th>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Contact Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              $sql = "SELECT * FROM sbo.emergency";
+              $result = mysqli_query($conn, $sql);
+              $resultCheck = mysqli_num_rows($result);
 
-<hr>
-<h1>Emergency Contact Section</h1>
-    <form class="" action="inc/insert.inc.php" method="post">
-      <ul>
-        <li>
-          <label for="">Last Name</label>
-          <input type="text" name="em-lname">
-        </li>
-        <li>
-          <label for="">First Name</label>
-          <input type="text" name="em-fname">
-        </li>
-        <li>
-          <label for="">Middle Name</label>
-          <input type="text" name="em-mname">
-        </li>
-        <li>
-          <label for="">Contact Number</label>
-          <input type="text" name="em-num">
-        </li>
-        <li>
-          <label for="">Address</label>
-          <input type="text" name="em-address">
-        </li>
-        <li>
-          <button type="submit" name="em-save">Save Contact</button>
-        </li>
-      </ul>
-    </form>
+              if ($resultCheck > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                  echo '
+                    <tr>
+                      <td>'. $row['em_id'].'</td>
+                      <td>'. $row['last_name']. ', '. $row['first_name']. ' '. $row['middle_name'] . '</td>
+                      <td>'. $row['address'].'</td>
+                      <td>'. $row['contact_num'].'</td>
+                    </tr>';
+                }
+              }
+            ?>
+          </tbody>
 
-    <table id="emergency" class="display">
-      <thead>
-        <tr>
-          <th>Emergency Contact ID</th>
-          <th>Name</th>
-          <th>Address</th>
-          <th>Contact Number</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-          $sql = "SELECT * FROM sbo.emergency";
-          $result = mysqli_query($conn, $sql);
-          $resultCheck = mysqli_num_rows($result);
+        </table>
 
-          if ($resultCheck > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-              echo '
-                <tr>
-                  <td>'. $row['em_id'].'</td>
-                  <td>'. $row['last_name']. ', '. $row['first_name']. ' '. $row['middle_name'] . '</td>
-                  <td>'. $row['address'].'</td>
-                  <td>'. $row['contact_num'].'</td>
-                </tr>';
-            }
-          }
-        ?>
-      </tbody>
+        <hr>
+        <h1>Event Section</h1>
+        <form class="" action="inc/insert.inc.php" method="post">
+          <ul>
+            <li>
+              <label for="">Event Title</label>
+              <input type="text" name="title">
+            </li>
+            <li>
+              <label for="">Start</label>
+              <input type="date" name="start">
+            </li>
+            <li>
+              <label for="">End</label>
+              <input type="date" name="end">
+            </li>
+            <li>
+              <label for="">Description</label>
+              <textarea name="desc" rows="8" cols="80"></textarea>
+            </li>
+            <li>
+              <button type="submit" name="add-event">Save New Event</button>
+            </li>
+          </ul>
+        </form>
 
-    </table>
+        <table id="event" class="display">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Date of Event</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php
+              $sql = "SELECT * FROM sbo.event;";
+              $result = mysqli_query($conn, $sql);
+              $resultCheck = mysqli_num_rows($result);
 
-    <hr>
-    <h1>Event Section</h1>
-    <form class="" action="inc/insert.inc.php" method="post">
-      <ul>
-        <li>
-          <label for="">Event Title</label>
-          <input type="text" name="title">
-        </li>
-        <li>
-          <label for="">Start</label>
-          <input type="date" name="start">
-        </li>
-        <li>
-          <label for="">End</label>
-          <input type="date" name="end">
-        </li>
-        <li>
-          <label for="">Description</label>
-          <textarea name="desc" rows="8" cols="80"></textarea>
-        </li>
-        <li>
-          <button type="submit" name="add-event">Save New Event</button>
-        </li>
-      </ul>
-    </form>
-
-    <table id="event" class="display">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Date of Event</th>
-          <th>Description</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-          $sql = "SELECT * FROM sbo.event;";
-          $result = mysqli_query($conn, $sql);
-          $resultCheck = mysqli_num_rows($result);
-
-          if ($resultCheck > 0) {
-            while ($row = mysqli_fetch_assoc($result)) {
-              $dateEvent = date('M d Y', strtotime($row['start_date']));
-              echo '
-                <tr>
-                  <td><a href="eventdetails.php?id='. $row['event_id'] . '">'. $row['title'].'</a></td>
-                  <td>'. $dateEvent .'</td>
-                  <td>'. $row['description'].'</td>
-                  <td><a href="eventdetails.php?id='.
-                    $row['event_id'] .'">Edit</a></td>
-                </tr>';
-            }
-          }
-        ?>
-      </tbody>
+              if ($resultCheck > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                  $dateEvent = date('M d Y', strtotime($row['start_date']));
+                  echo '
+                    <tr>
+                      <td><a href="eventdetails.php?id='. $row['event_id'] . '">'. $row['title'].'</a></td>
+                      <td>'. $dateEvent .'</td>
+                      <td>'. $row['description'].'</td>
+                      <td><a href="eventdetails.php?id='.
+                        $row['event_id'] .'">Edit</a></td>
+                    </tr>';
+                }
+              }
+            ?>
+          </tbody>
+        </table>
+      </div>
 
 
+    </div>
 
-    </table>
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
     <script>
